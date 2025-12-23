7debfb8150bbea34187f4c211f32fb4a6ecf71ff
asifsaascraft <asifsaascraft@gmail.com>
2025-12-23 14:13:11 +0530

    Removed dynamic status from course

--- models/Course.js
+++ models/Course.js
@@ -1,6 +1,5 @@
 // models/Event.js
 import mongoose from "mongoose";
-import moment from "moment-timezone";
 
 // Schema
 const CourseSchema = new mongoose.Schema(
@@ -84,30 +83,6 @@ CourseSchema.pre("findOneAndUpdate", function (next) {
   next();
 });
 
-/**
- * Virtual: Dynamic event status
- */
-CourseSchema.virtual("dynamicStatus").get(function () {
-  const tz = this.timeZone || "UTC";
-
-  const start = moment.tz(
-    `${this.startDate} ${this.startTime}`,
-    "DD/MM/YYYY hh:mm A",
-    tz
-  );
-
-  const end = moment.tz(
-    `${this.endDate} ${this.endTime}`,
-    "DD/MM/YYYY hh:mm A",
-    tz
-  );
-
-  const now = moment.tz(tz);
-
-  if (now.isBefore(start)) return "Upcoming";
-  if (now.isBetween(start, end, null, "[]")) return "Live";
-  return "Past";
-});
 
 // Avoid model overwrite during hot-reload
 export default mongoose.models.Course ||
